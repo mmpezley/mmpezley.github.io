4b6efaa78835344edc186e48ed049f70151cde6c
mmpezley <63078995+mmpezley@users.noreply.github.com>
2024-01-04 00:01:36 -0600

    testing new portfolio

---
+++ resumePortfolio2024/resume/button_actions.js
@@ -0,0 +1,133 @@
+function hoverAftermarket() {
+	document.getElementById("aftermarketMoreCol").style.display = "none";
+	document.getElementById("aftermarketMoreColHover").style.display = "inline";
+	document.getElementById("aftermarketImageCol").style.display = "none";
+	document.getElementById("aftermarketImageColHover").style.display = "block";
+
+}
+
+function leaveAftermarket() {
+	document.getElementById("aftermarketMoreCol").style.display = "inline";
+	document.getElementById("aftermarketMoreColHover").style.display = "none";
+	document.getElementById("aftermarketImageCol").style.display = "block";
+	document.getElementById("aftermarketImageColHover").style.display = "none";
+}
+
+function hoverRobotics() {
+	document.getElementById("roboticsMoreCol").style.display = "none";
+	document.getElementById("roboticsMoreColHover").style.display = "inline";
+	document.getElementById("roboticsImageCol").style.display = "none";
+	document.getElementById("roboticsImageColHover").style.display = "block";
+}
+
+function leaveRobotics() {
+	document.getElementById("roboticsMoreCol").style.display = "inline";
+	document.getElementById("roboticsMoreColHover").style.display = "none";
+	document.getElementById("roboticsImageCol").style.display = "block";
+	document.getElementById("roboticsImageColHover").style.display = "none";
+}
+
+function hoverScorecard() {
+	document.getElementById("cpsTraceMoreCol").style.display = "none";
+	document.getElementById("cpsTraceMoreColHover").style.display = "inline";
+	document.getElementById("cpsTraceImageCol").style.display = "none";
+	document.getElementById("cpsTraceImageColHover").style.display = "block";
+}
+
+function leaveScorecard() {
+	document.getElementById("cpsTraceMoreCol").style.display = "inline";
+	document.getElementById("cpsTraceMoreColHover").style.display = "none";
+	document.getElementById("cpsTraceImageCol").style.display = "block";
+	document.getElementById("cpsTraceImageColHover").style.display = "none";
+}
+
+function hoverTextToImageModel() {
+	document.getElementById("textToImageImageCol2").style.display = "none";
+	document.getElementById("textToImageImageColHover2").style.display = "inline";
+	document.getElementById("textToImageMoreCol2").style.display = "none";
+	document.getElementById("textToImageMoreColHover2").style.display = "block";
+}
+
+function leaveTextToImageModel() {
+	document.getElementById("textToImageImageCol2").style.display = "inline";
+	document.getElementById("textToImageImageColHover2").style.display = "none";
+	document.getElementById("textToImageMoreCol2").style.display = "block";
+	document.getElementById("textToImageMoreColHover2").style.display = "none";
+}
+
+function goToScorecard() {
+	window.alert("TEST");
+	location.replace("johndeerescorecard.html");
+}
+
+function hoverScorecardExperiences() {
+	document.getElementById("scorecardLinkExperiencesMore").style.display = "none";
+	document.getElementById("scorecardLinkExperiencesMoreHover").style.display = "inline";
+	document.getElementById("scorecardLinkExperiencesImage").style.display = "none";
+	document.getElementById("scorecardLinkExperiencesImageHover").style.display = "block";
+}
+
+function leaveScorecardExperiences() {
+	document.getElementById("scorecardLinkExperiencesMore").style.display = "block";
+	document.getElementById("scorecardLinkExperiencesMoreHover").style.display = "none";
+	document.getElementById("scorecardLinkExperiencesImage").style.display = "block";
+	document.getElementById("scorecardLinkExperiencesImageHover").style.display = "none";
+} 
+
+function hoverAftermarketExperiences() {
+	document.getElementById("aftermarketLinkExperiencesMore").style.display = "none";
+	document.getElementById("aftermarketLinkExperiencesMoreHover").style.display = "inline";
+	document.getElementById("aftermarketLinkExperiencesImage").style.display = "none";
+	document.getElementById("aftermarketLinkExperiencesImageHover").style.display = "block";
+}
+
+function leaveAftermarketExperiences() {
+	document.getElementById("aftermarketLinkExperiencesMore").style.display = "block";
+	document.getElementById("aftermarketLinkExperiencesMoreHover").style.display = "none";
+	document.getElementById("aftermarketLinkExperiencesImage").style.display = "block";
+	document.getElementById("aftermarketLinkExperiencesImageHover").style.display = "none";
+} 
+
+function hoverRoboticsExperiences() {
+	document.getElementById("roboticsLinkExperiencesMore").style.display = "none";
+	document.getElementById("roboticsLinkExperiencesMoreHover").style.display = "inline";
+	document.getElementById("roboticsLinkExperiencesImage").style.display = "none";
+	document.getElementById("roboticsLinkExperiencesImageHover").style.display = "block";
+}
+
+function leaveRoboticsExperiences() {
+	document.getElementById("roboticsLinkExperiencesMore").style.display = "block";
+	document.getElementById("roboticsLinkExperiencesMoreHover").style.display = "none";
+	document.getElementById("roboticsLinkExperiencesImage").style.display = "block";
+	document.getElementById("roboticsLinkExperiencesImageHover").style.display = "none";
+} 
+
+function hoverEmail() {
+	document.getElementById("emailBlack").style.display = "none";
+	document.getElementById("emailPink").style.display = "block";
+}
+
+function leaveEmail() {
+	document.getElementById("emailBlack").style.display = "block";
+	document.getElementById("emailPink").style.display = "none";
+} 
+
+function hoverPhone() {
+	document.getElementById("phoneBlack").style.display = "none";
+	document.getElementById("phonePink").style.display = "block";
+}
+
+function leavePhone() {
+	document.getElementById("phoneBlack").style.display = "block";
+	document.getElementById("phonePink").style.display = "none";
+} 
+
+function hoverLinkedIn() {
+	document.getElementById("linkedinBlack").style.display = "none";
+	document.getElementById("linkedinPink").style.display = "block";
+}
+
+function leaveLinkedIn() {
+	document.getElementById("linkedinBlack").style.display = "block";
+	document.getElementById("linkedinPink").style.display = "none";
+} 
